1c5299d14bf580a95b94dcd7ae2f541835f20a8f
nickpodratz <nick@podratz.de>
2024-03-04 23:22:55 +0100

    Implement copy confirmation toast

--- app/src/main/java/de/podratz/software/binarify/android/MainActivity.kt
+++ app/src/main/java/de/podratz/software/binarify/android/MainActivity.kt
@@ -2,10 +2,25 @@ package de.podratz.software.binarify.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import de.podratz.software.binarify.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+        val copyButton = binding.copyButton
+        copyButton.setOnClickListener {
+            val confirmation = resources.getString(R.string.copy_confirmation)
+            val confirmationToast = Toast.makeText(this, confirmation, Toast.LENGTH_SHORT)
+            confirmationToast.show()
+        }
     }
 }
