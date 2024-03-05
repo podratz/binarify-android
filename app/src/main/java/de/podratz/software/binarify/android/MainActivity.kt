77c9967c31385b5afa9b3dc1041a4d9de416d31f
nickpodratz <nick@podratz.de>
2024-03-05 13:09:43 +0100

    Implement copy to clipboard

--- app/src/main/java/de/podratz/software/binarify/android/MainActivity.kt
+++ app/src/main/java/de/podratz/software/binarify/android/MainActivity.kt
@@ -1,10 +1,16 @@
 package de.podratz.software.binarify.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
+import android.R.attr.label
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import de.podratz.software.binarify.android.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,21 +21,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         val view = binding.root
+        val inputEditText = binding.inputEditText
+        val outputTextView = binding.outputTextView
+        val copyButton = binding.copyButton
+
         setContentView(view)
 
-        val copyButton = binding.copyButton
-        copyButton.setOnClickListener {
-            val confirmation = resources.getString(R.string.copy_confirmation)
-            val confirmationToast = Toast.makeText(this, confirmation, Toast.LENGTH_SHORT)
-            confirmationToast.show()
-        }
-
-        val outputTextView = binding.outputTextView
-
-        val inputEditText = binding.inputEditText
         inputEditText.doOnTextChanged { charSequence, _, _, _ ->
             val input = charSequence.toString()
             outputTextView.text = toBinary(input)
+        }
+
+        copyButton.setOnClickListener {
+            val outputText = outputTextView.text.toString()
+            copyToClipboard(outputText)
+
+            val confirmation = resources.getString(R.string.copy_confirmation)
+            val confirmationToast = Toast.makeText(this, confirmation, Toast.LENGTH_SHORT)
+            confirmationToast.show()
         }
     }
 
@@ -38,5 +47,11 @@ class MainActivity : AppCompatActivity() {
         val byteArray = text.toByteArray(Charsets.UTF_8)
         val bitArray = byteArray.map { it.toString(2) }
         return bitArray.reduce { acc, char -> acc + char }
+    }
+
+    private fun copyToClipboard(text: String) {
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipData = ClipData.newPlainText(null, text)
+        clipboardManager.setPrimaryClip(clipData)
     }
 }
