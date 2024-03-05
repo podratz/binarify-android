a7d4eea7c24947a2a283b7046136e1fb843c0b22
nickpodratz <nick@podratz.de>
2024-03-05 12:59:40 +0100

    Implement translation to binary

--- app/src/main/java/de/podratz/software/binarify/android/MainActivity.kt
+++ app/src/main/java/de/podratz/software/binarify/android/MainActivity.kt
@@ -3,6 +3,7 @@ package de.podratz.software.binarify.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import de.podratz.software.binarify.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,5 +23,20 @@ class MainActivity : AppCompatActivity() {
             val confirmationToast = Toast.makeText(this, confirmation, Toast.LENGTH_SHORT)
             confirmationToast.show()
         }
+
+        val outputTextView = binding.outputTextView
+
+        val inputEditText = binding.inputEditText
+        inputEditText.doOnTextChanged { charSequence, _, _, _ ->
+            val input = charSequence.toString()
+            outputTextView.text = toBinary(input)
+        }
+    }
+
+    private fun toBinary(text: String) : String {
+        if (text.isEmpty()) return ""
+        val byteArray = text.toByteArray(Charsets.UTF_8)
+        val bitArray = byteArray.map { it.toString(2) }
+        return bitArray.reduce { acc, char -> acc + char }
     }
 }
